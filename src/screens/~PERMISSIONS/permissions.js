c0d404d50f7c3615a6a7d6e38aa37414b593f55d
Tiffany Wise <TiffanyJWise84@gmail.com>
2018-04-21 09:42:14 -0400

    Adding placeholder screens from Settings subscreens

---
+++ src/screens/~PERMISSIONS/permissions.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+import {
+    Container,
+    Header, Left, Body, Right,
+    Content,
+    Button,
+    Icon,
+    Card, CardItem,
+    Text,
+
+} from "native-base";
+import { Row, Col, Grid } from 'react-native-easy-grid';
+import { ImageBackground, View, TextInput } from 'react-native';
+import styles from "./styles";
+
+const screenBackground = require("../../../assets/Semita-background-tracks.png");
+
+
+
+class Permissions extends Component {
+    render() {
+        return (
+            <Container style={styles.container}>
+
+
+                <Grid style={styles.header}>
+                    <Col >
+
+                        <Button
+                            transparent
+                            onPress={() => this.props.navigation.navigate("Home")}
+                        >
+                            <Icon style={{ color: 'white' }} active name="home" />
+                        </Button>
+                    </Col>
+                    <Col>
+
+                        <Button
+                            transparent
+                            style={styles.button}
+                            onPress={() => this.props.navigation.navigate("Settings")}
+                        >
+                            <Icon style={{ color: 'white' }} active name="settings" />
+                        </Button>
+                    </Col>
+                </Grid>
+
+                <ImageBackground
+                    source={screenBackground}
+                    style={styles.image}
+                >
+
+
+
+
+                    <View style={styles.main_window}>
+                        <Text>Permissions Screen</Text>
+                    </View>
+
+
+
+                </ImageBackground>
+            </Container>
+        );
+    }
+}
+
+export default Permissions;
---
+++ src/screens/~USER INFORMATION/styles.js
@@ -0,0 +1,20 @@
+export default {
+
+    
+   
+    header: {
+        backgroundColor: 'rgba(1,1,1,0.8)',
+
+    },
+
+    button: {
+        justifyContent: 'center',
+        alignSelf: 'flex-end'
+    },
+    image: {
+        flex: 9
+    },
+    main_window: {
+        alignItems: 'center'
+    }
+}
